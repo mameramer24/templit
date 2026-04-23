d5d423d4beab276cc42b95b897d5ff3ab2d69179
mameramer24 <mameramer24@gmail.com>
2026-04-23 03:04:42 +0300

    fix: correct typo setSelectedLayerId to setSelectedId in canvas-editor.tsx

--- src/components/builder/canvas-editor.tsx
+++ src/components/builder/canvas-editor.tsx
@@ -333,7 +333,7 @@ export default function CanvasEditor({
         src: url,
       };
       setLayers([...layers, newLayer]);
-      setSelectedLayerId(newLayer.id);
+      setSelectedId(newLayer.id);
     };
   };
 
